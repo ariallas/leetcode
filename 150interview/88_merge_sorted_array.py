af7e8405823b6240d64605acfa87662deb708266
Ilya Reznikov <ril.xilus@gmail.com>
2024-04-28 20:21:47 +0300

    Added poetry and ruff setup

---
+++ 150interview/88_merge_sorted_array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def run(self) -> None:
+        self.merge(
+            [1, 2, 3, 0, 0, 0],
+            3,
+            [2, 5, 6],
+            3,
+        )
+
+    def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+
+
+Solution().run()
